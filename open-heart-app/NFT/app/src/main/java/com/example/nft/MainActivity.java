fe296e442cba143376ec802bcda7bbe26c89c7e2
RafaelJo17~ <672019090@student.uksw.edu>
2022-04-13 19:11:23 +0700

    App NFT eps2

--- open-heart-app/NFT/app/src/main/java/com/example/nft/MainActivity.java
+++ open-heart-app/NFT/app/src/main/java/com/example/nft/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.nft;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView logo;
+    TextInputLayout email, password;
+    Button btnLogin;
+    TextView dont, click;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +32,34 @@ public class MainActivity extends AppCompatActivity {
 
         logo = findViewById(R.id.logo);
 
-        logo.animate().translationY(-1600).setDuration(1000).setStartDelay(3000);
+        email = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+
+        dont = findViewById(R.id.donthave);
+        click = findViewById(R.id.clickhere);
+
+        btnLogin = findViewById(R.id.login);
+
+
+        logo.animate().translationY(-600).setDuration(1000).setStartDelay(1000);
+
+        Animation animFadeIn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fadein);
+
+        email.startAnimation(animFadeIn);
+        password.startAnimation(animFadeIn);
+        btnLogin.startAnimation(animFadeIn);
+        dont.startAnimation(animFadeIn);
+        click.startAnimation(animFadeIn);
+
+
+
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//
+//            }
+//        }, 2000);
 
 
 
